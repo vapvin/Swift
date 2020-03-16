8e61fbfe3bee6a3797885bccf961160c9975f6c3
vapvin <tpehdj12@daum.net>
2020-03-16 15:46:35 +0900

    repeat and while

--- Home.playground/Contents.swift
+++ Home.playground/Contents.swift
@@ -7,10 +7,19 @@ import UIKit
 
 var i = 0;
 
+print("\n -------While-------- \n")
 while i < 10 {
     print(i + 1)
-    if i==5{
-        break
-    }
+//    if i==5{
+//        break
+//    }
     i += 1
 }
+
+// repeat looklike other language do ~ while case
+print("\n -------Repeat------- \n")
+i = 0
+repeat {
+    print(i)
+    i += 1
+} while i < 10
